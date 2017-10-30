baea757157547f1c3d3866339617bdbb643f7603
三点 <zousandian@gmail.com>
2017-10-30 11:34:09 +0800

    Update delete temp code

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-/*var Readable = require('stream').Readable;*/
-//var rs = Readable();
-
-//var c = 97 - 1;
-
-//rs._read = function () {
-      //if (c >= 'z'.charCodeAt(0)) return rs.push(null);
-          
-      //setTimeout(function () {
-                //rs.push('我a', 'ascii');
-                    
-      //}, 100);
-
-//};
-
-//rs.pipe(process.stdout);
-
-//process.on('exit', function () {
-      //console.error('\n_read() called ' + (c - 97) + ' times');
-
-//});
-//process.stdout.on('error', process.exit);
-
-
-
 const fs = require('fs')
 const Readable = require('stream').Readable
 
